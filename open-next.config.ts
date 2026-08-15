8c3dc83b793c4d040dc4c948f8f2bb574c28b5aa
jay home <jaime.home@gmail.com>
2026-08-16 00:37:14 +0100

    Fix open-next config newline formatting properly

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,1 +1,3 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";`n`nexport default defineCloudflareConfig({});
+import { defineCloudflareConfig } from '@opennextjs/cloudflare';
+
+export default defineCloudflareConfig({});
