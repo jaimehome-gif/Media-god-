8c73036dcadc119c3e4534cf51552d60ad2724e5
jaimehome-gif <jaime.home@gmail.com>
2026-08-31 14:01:04 +0100

    Add service worker for asset caching
    Implement service worker for caching assets.

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'media-god-v1';
+const ASSETS = [
+  '/',
+  '/live-tv',
+  '/watchlist',
+  '/favorites',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
